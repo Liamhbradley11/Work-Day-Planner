218d5233e3f47d9202dbd2c5b5f1026a79489a53
Liam Bradley <liamhbradley11@gmail.com>
2020-09-24 23:12:38 -0700

    appended the date and time to the page

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,23 @@
-console.log("hello");
+var presentDay = (moment().format("MMMM Do YYYY"));
+$("currentDay").text(presentDay);
+console.log(presentDay)
 
+//append the date and time to the top of the page
+document.getElementById("currentDay").append(presentDay);
+
+
+
+
+// moment().format("MMMM Do YYYY")
+// moment().format("LL");
+
+// console.log("hello");
+
+//add event listener to the save button
 $(".saveBtn").click(function(){
     var value = ($(this).siblings(".desciption").val())
     var key = ($(this).parent().attr("id"))
     console.log(key, value);
     })
+
+    //

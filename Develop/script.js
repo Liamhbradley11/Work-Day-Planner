0df6e661563ea6f1b6ee31cff82cf856e9d05aea
Liam Bradley <liamhbradley11@gmail.com>
2020-09-26 17:17:25 -0700

    added save icon to button

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,28 @@
+
+
+
 var presentDay = (moment().format("MMMM Do YYYY"));
 $("currentDay").text(presentDay);
-console.log(presentDay)
-
-//append the date and time to the top of the page
 document.getElementById("currentDay").append(presentDay);
 
+//llop through past preset and future times
+$(document).ready(function() {
+for (var i = 9; i < 18; i++){
+   if (i === moment().hour()){
+        $("#h-" + i).removeClass("past, present, future").addClass("present");
+   } else if (i < moment().hour()) {
+        $("#h-" + i).removeClass("past, present, future").addClass("past");
+    } else {
+        $("#h-" + i).removeClass("past, present, future").addClass("future");
+        }
+     }
+
+    }, 1000);
+// console.log(presentDay)
+
+//append the date and time to the top of the page
+
+//time-block
 
 //add event listener to the save button
 $(".saveBtn").click(function(){
@@ -23,4 +41,3 @@ $(".saveBtn").click(function(){
 
 
 
-   
